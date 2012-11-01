f0b493adb86c454aba94e80c6e8f687d2553c756
Joe Gibbs Politz <joe@cs.brown.edu>
2012-11-01 10:47:31 -0400

    environments

--- python-interp.rkt
+++ python-interp.rkt
@@ -28,8 +28,8 @@
     [CApp (fun arges)
      (type-case CVal (interp-env fun env)
        [VClosure (cenv argxs body)
-         (local [(define argvs (map (lambda (e) (interp-env e cenv)) arges))]
-          (interp-env body (bind-args argxs argvs env)))]
+         (local [(define argvs (map (lambda (e) (interp-env e env)) arges))]
+          (interp-env body (bind-args argxs argvs cenv)))]
        [else (error 'interp "Not a closure")])]
 
     [CFunc (args body) (VClosure env args body)] 
